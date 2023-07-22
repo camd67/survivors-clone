58497c8a05b03ebb796180bb1fda9facbe066d9f
CamD67 <7773345+camd67@users.noreply.github.com>
2023-07-22 15:21:35 -0700

    Lesson 32: Fix enemy spawning

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -16,6 +16,32 @@ func _ready():
 	arena_time_manager.area_difficulty_incrased.connect(on_arena_difficulty_increased)
 
 
+func get_spawn_position():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player == null:
+		return Vector2.ZERO
+	
+	var spawn_position = Vector2.ZERO
+	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	
+	# Rotate the angle 4 times to guarantee we get a valid spawn position
+	for i in 4:
+		# Spawn an enemy in a random location around a big circle around the player
+		# That way the player won't see the spawn
+		
+		spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+		
+		var query_parameters = PhysicsRayQueryParameters2D.create(player.global_position, spawn_position, 1)
+		var result = get_tree().root.world_2d.direct_space_state.intersect_ray(query_parameters)
+		if result.is_empty():
+			# We are clear!
+			break
+		else:
+			random_direction = random_direction.rotated(deg_to_rad(90))
+	
+	return spawn_position
+
+
 func on_timer_timeout():
 	timer.start()
 	
@@ -23,15 +49,10 @@ func on_timer_timeout():
 	if player == null:
 		return
 	
-	# Spawn an enemy in a random location around a big circle around the player
-	# That way the player won't see the spawn
-	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
-	var spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
-	
 	var enemy = basic_enemy_scene.instantiate() as Node2D
 	var entity_layer = get_tree().get_first_node_in_group("entities_layer")
 	entity_layer.add_child(enemy)
-	enemy.global_position = spawn_position
+	enemy.global_position = get_spawn_position()
 
 
 func on_arena_difficulty_increased(arena_difficulty: int):
