7dfe11e4cb6f8466d5c8c280529aa67a544d7c89
CamD67 <7773345+camd67@users.noreply.github.com>
2023-07-21 21:04:04 -0700

    Lesson 29: Defeat screen

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@export var victory_screen_scene: PackedScene
+@export var end_screen_scene: PackedScene
 
 @onready var timer = $Timer
 
@@ -12,5 +12,5 @@ func get_time_elapsed():
 
 
 func on_timer_timeout():
-	var victory_screen = victory_screen_scene.instantiate()
-	add_child(victory_screen)
+	var end_screen = end_screen_scene.instantiate()
+	add_child(end_screen)
