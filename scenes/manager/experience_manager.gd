10bac2493f13acb172079b84b0c44891dac2ec4e
CamD67 <7773345+camd67@users.noreply.github.com>
2023-07-21 19:19:06 -0700

    Lesson 20: Experience bar

--- scenes/manager/experience_manager.gd
+++ scenes/manager/experience_manager.gd
@@ -1,15 +1,26 @@
 extends Node
+class_name ExperienceManager
 
+signal experience_updated(current_experieince: float, target_experieince: float)
+
+const TARGET_EXPERIENCE_GROWTH = 5
 
 var current_experience: float = 0
+var current_level: float = 1
+var target_experience: float = 5
 
 func _ready():
 	GameEvents.experience_vial_collected.connect(on_experience_vial_collected)
 
 
 func increment_experience(number: float) -> void:
-	current_experience += number
-	print(current_experience)
+	current_experience = min(current_experience + number, target_experience)
+	experience_updated.emit(current_experience, target_experience)
+	if current_experience == target_experience:
+		current_level += 1
+		target_experience += TARGET_EXPERIENCE_GROWTH
+		current_experience = 0
+		experience_updated.emit(current_experience, target_experience)
 
 
 func on_experience_vial_collected(number: float):
