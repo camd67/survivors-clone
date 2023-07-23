dc679f0ecd59f18f64ebb036a93d4d6af0278bd7
CamD67 <7773345+camd67@users.noreply.github.com>
2023-07-22 20:43:24 -0700

    Lesson 36: Player animation

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 @onready var health_component: HealthComponent = $HealthComponent
 @onready var health_bar = $HealthBar
 @onready var abilities = $Abilities
+@onready var animation_player = $AnimationPlayer
+@onready var visuals = $Visuals
 
 const MAX_SPEED = 125
 const ACCELERATION_SMOOTHING = 25
@@ -27,6 +29,17 @@ func _process(delta):
 	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
 	
 	move_and_slide()
+	
+	if movement_vector.x != 0 || movement_vector.y != 0:
+		animation_player.play("walk")
+	else:
+		animation_player.play("RESET")
+	
+	var move_sign = sign(movement_vector.x)
+	if move_sign == 0:
+		visuals.scale = Vector2.ONE
+	else:
+		visuals.scale = Vector2(move_sign, 1)
 
 
 func get_movement_vector():
