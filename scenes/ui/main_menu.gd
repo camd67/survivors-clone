ea2f1bde16759c0e38b23783ccd9a9be1c5148dd
CamD67 <7773345+camd67@users.noreply.github.com>
2023-07-24 17:18:12 -0700

    Lesson 63: Options menu

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -1,5 +1,6 @@
 extends CanvasLayer
 
+var options_scene = preload("res://scenes/ui/options_menu.tscn")
 
 func _ready():
 	%PlayButton.pressed.connect(on_play_pressed)
@@ -12,8 +13,14 @@ func on_play_pressed():
 
 
 func on_options_pressed():
-	pass
+	var options = options_scene.instantiate()
+	add_child(options)
+	options.back_pressed.connect(on_options_closed.bind(options))
 
 
 func on_quit_pressed():
 	get_tree().quit()
+
+
+func on_options_closed(options: Node):
+	options.queue_free()
---
+++ scenes/ui/options_menu.gd
@@ -0,0 +1,53 @@
+extends CanvasLayer
+
+signal back_pressed
+
+@onready var sfx_slider: Slider = %SfxSlider
+@onready var music_slider: Slider = %MusicSlider
+@onready var window_button: Button = %WindowButton
+@onready var back_button = %BackButton
+
+func _ready() -> void:
+	window_button.pressed.connect(on_window_button_pressed)
+	sfx_slider.value_changed.connect(audio_slider_changed.bind("Sfx"))
+	music_slider.value_changed.connect(audio_slider_changed.bind( "Music"))
+	back_button.pressed.connect(on_back_pressed)
+	update_display()
+
+
+func update_display() -> void:
+	window_button.text = "Windowed"
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		window_button.text = "Fullscreen"
+	
+	sfx_slider.value = get_bus_volume_percent("Sfx")
+	music_slider.value = get_bus_volume_percent("Music")
+
+
+func get_bus_volume_percent(bus_name: String) -> float:
+	var bus_index = AudioServer.get_bus_index(bus_name)
+	var volume_db = AudioServer.get_bus_volume_db(bus_index)
+	return db_to_linear(volume_db)
+
+
+func set_bus_volume_percent(bus_name: String, percent: float) -> void:
+	var bus_index = AudioServer.get_bus_index(bus_name)
+	var volume_db = linear_to_db(percent)
+	AudioServer.set_bus_volume_db(bus_index, volume_db)
+
+
+func on_window_button_pressed() -> void:
+	var mode = DisplayServer.window_get_mode()
+	if mode != DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	update_display()
+
+
+func audio_slider_changed(value: float, bus_name: String) -> void:
+	set_bus_volume_percent(bus_name, value)
+
+
+func on_back_pressed():
+	back_pressed.emit()
