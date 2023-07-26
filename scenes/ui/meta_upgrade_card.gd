3eb8193cc890afeba21f6c0798a97bf5d4bf5f74
CamD67 <7773345+camd67@users.noreply.github.com>
2023-07-25 18:01:17 -0700

    Lesson 69: Meta upgrade card

---
+++ scenes/ui/meta_upgrade_card.gd
@@ -0,0 +1,27 @@
+extends PanelContainer
+class_name MetaUpgradeCard
+
+signal selected
+
+@onready var name_label: Label = %NameLabel
+@onready var description_label: Label = %DescriptionLabel
+
+
+func _ready():
+	gui_input.connect(on_gui_input)
+
+
+func set_meta_upgrade(upgrade: MetaUpgrade) -> void:
+	name_label.text = upgrade.name
+	description_label.text = upgrade.description
+
+
+func select_card():
+	$AnimationPlayer.play("selected")
+
+
+func on_gui_input(event: InputEvent):
+
+	if event.is_action_pressed("left_click"):
+		select_card()
+
